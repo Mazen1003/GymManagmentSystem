b48952d34a00de53e237a17f7947b0c9997a0f35
Mazen1003 <mazen_alamin@hotmail.com>
2026-01-22 12:05:52 +0100

    Add comments for table creation and normalization
    Added comments to explain table creation and constraints.

--- SQL Queries/02_CreateTables.sql
+++ SQL Queries/02_CreateTables.sql
@@ -1,1 +1,4 @@
-
+Denna fil skapar alla tabeller enligt ERD:
+-- MEMBER, TRAINER, SESSION, MEMBERSHIP, PAYMENT, BOOKING
+-- PK, FK, CHECK, DEFAULT och UNIQUE constraints finns
+-- Tabellerna är normaliserade enligt 3NF
